e4a1158a0d3c169d3b1c73ebd07c242126e3d8f4
Oyinkay <you@example.com>
2023-03-22 13:15:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - to print the statement 
+ * main - to print the statement
  * Return: 1 (to print print out error)
  */
 
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
