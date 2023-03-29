2776da6d3cb8f0a0ba21f2f6ff67011c9b272b5b
Oyinkay <you@example.com>
2023-03-29 07:39:43 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Description: The program is to print alphabet in lower case
+ * Return: Always return 0 (success)
+*/
+int main(void)
+{
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+
+	return (0);
+}
