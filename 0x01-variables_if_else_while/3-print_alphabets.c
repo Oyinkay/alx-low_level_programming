164677949bcaac6e6764d001851bad21715d4911
Oyinkay <you@example.com>
2023-03-29 10:17:16 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: The program would write alphabets in lower and upper case
+ * Return: Always return 0 (success)
+*/
+
+int main(void)
+
+{
+	char l
+	char u
+
+	for (i = 'a' ; i <= 'z' ; ++i; u = 'A' ; u <= 'Z' ; ++u)
+	putchar(i, u);
+	putchar('\n')
+	return (0)
+}
