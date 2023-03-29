e8c0a910d9b5feb7666a58e031b28d78644bfc33
Oyinkay <you@example.com>
2023-03-29 11:42:05 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	char i
+	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
 	if (i != 'e' && i != 'q')
 	putchar(i);
-	putchar('\n\)
-			return (0);
-			}
+	putchar('\n')
+	return (0);
+}			}
