3e1d18d4678fea6407e5c9f6c00d7de3be8870cc
Oyinkay <you@example.com>
2023-04-03 11:25:14 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: The program writes alphabet in reverse move
+ * return - always positive 0 (successful)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar("\n")
+}
