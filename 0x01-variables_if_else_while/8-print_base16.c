57e201ad3ec99f7b479451fe88793de1b22ca7be
Oyinkay <you@example.com>
2023-04-03 11:53:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: The program will print all hexadecimal number
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+	char a
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i = '0');
+	for (a = 'a' ; a <= 'f' a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
