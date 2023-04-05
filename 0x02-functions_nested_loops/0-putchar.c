517028adb8b000c5b2255bb61e58973c181910ab
Oyinkay <you@example.com>
2023-04-05 11:29:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * Retur: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
