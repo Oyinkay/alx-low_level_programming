1a36c58fb3c783fc2b1e4e2d92ee46c606264f1c
Oyinkay <you@example.com>
2023-04-19 09:52:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
 */
 
 void print_alphabet_x10(void)
-{
+
 	int count = 0;
 	char letter;
 
@@ -14,6 +14,6 @@ void print_alphabet_x10(void)
 	{
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-		_putchar('\n');
+	_putchar('\n');
 	}
-}
+
